e36297f3647dcddfadbf71d8f58de266b58c46b6
endu <futoshi.endo@pepabo.com>
2022-02-26 15:46:46 +0900

    fix

--- main.go
+++ main.go
@@ -35,12 +35,12 @@ func FizBuz(in string) (string, error) {
 	case i%15 == 0:
 		// 15の倍数ならFizzBuzz
 		return "FizzBuzz", nil
-	}
 	case i%3 == 0:
 		// 3の倍数ならFizz
 		return "Fizz", nil
 	case i%5 == 0:
 		// 5の倍数ならBuzz
 		return "Buzz", nil
+	}
 	return "", fmt.Errorf("判定できませんでした: %s", in)
 }
